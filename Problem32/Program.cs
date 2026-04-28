55bf1b173441e9dda797513e98c070fca17a4b53
vanessapopovici <vanessamariapopovici@gmail.com>
2026-04-28 21:47:56 +0300

    Add problem 32.

---
+++ Problem32/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem32
+{
+    public class Solution
+    {
+        public int LongestValidParentheses(string s)
+        {
+            int max = 0;
+            Stack<int> stack = new Stack<int>();
+            stack.Push(-1);
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    stack.Pop();
+
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(i);
+                    }
+                    else
+                    {
+                        max = Math.Max(max, i - stack.Peek());
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
