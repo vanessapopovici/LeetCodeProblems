280b6afab8301a390cb2e9d499c6b22cfcb8099a
vanessapopovici <vanessamariapopovici@gmail.com>
2026-05-04 15:14:53 +0300

    Add problem 1 and 231.

---
+++ Problem231/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        return n == 1 ? true : n % 2 != 0 ? false : IsPowerOfTwo(n / 2);
+    }
+}
