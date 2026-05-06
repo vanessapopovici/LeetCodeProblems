6655e04ad6ea013c339bae540a7908d8f0e36f8e
vanessapopovici <vanessamariapopovici@gmail.com>
2026-05-06 21:11:01 +0300

    Add problem 26.

---
+++ Problem13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem13
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>
+            {
+                {'I', 1 },
+                {'V', 5 },
+                {'X', 10 },
+                {'L', 50 },
+                {'C', 100 },
+                {'D', 500 },
+                {'M', 1000 }
+            };
+
+            int total = 0;
+            int n = s.Length;
+
+            for(int i = 0; i < n; i++)
+            {
+                int currentVal = dictionary[s[i]];
+                if(i + 1 < n && currentVal < dictionary[s[i + 1]])
+                    total -= currentVal;
+                else
+                    total += currentVal;
+            }
+            return total;
+        }
+    }
+}
---
+++ Problem26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem26
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int i = 0;
+
+            for(int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
+    }
+}
