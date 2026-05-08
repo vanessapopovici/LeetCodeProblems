a11ef5cabbe570dfa5bb4c63320f7a3300a9c7c3
vanessapopovici <vanessamariapopovici@gmail.com>
2026-05-08 20:39:22 +0300

    Add problem 26.

---
+++ Problem22/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateParenthesis(3);
+        }
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            var result = new List<string>();
+
+            var sb = new StringBuilder();
+
+            Backtrack(result, sb, 0, 0, n);
+            return result;
+        }
+        public static void Backtrack(IList<string> result, StringBuilder sb, int open, int close, int max)
+        {
+            if (sb.Length == max * 2)
+            {
+                result.Add(sb.ToString());
+                return;
+            }
+
+            if (open < max)
+            {
+                sb.Append("(");
+                Backtrack(result, sb, open + 1, close, max);
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+            if (close < open)
+            {
+                sb.Append(")");
+                Backtrack(result, sb, open, close + 1, max);
+                sb.Remove(sb.Length - 1, 1);
+            }
+        }
+    }
+}
---
+++ Problem28/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem28
+{
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if(needle.Length == 0)
+                return 0;
+
+            int n = haystack.Length;
+            int m = needle.Length;
+
+            for (int i = 0; i <= n - m; i++)
+            {
+                int j = 0;
+
+                while (j < m && haystack[i + j] == needle[j])
+                    j++;
+
+                if (j == m)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
